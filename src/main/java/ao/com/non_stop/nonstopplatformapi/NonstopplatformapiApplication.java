d4663fee32aa0ead60623281c05ab237d12cff6d
Dário Silva <dariosilva13222@gmail.com>
2024-09-20 12:11:10 +0100

    inits springboot project

---
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/NonstopplatformapiApplication.java
@@ -0,0 +1,13 @@
+package ao.com.non_stop.nonstopplatformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonstopplatformapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonstopplatformapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/com/non_stop/nonstopplatformapi/NonstopplatformapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.com.non_stop.nonstopplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonstopplatformapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
