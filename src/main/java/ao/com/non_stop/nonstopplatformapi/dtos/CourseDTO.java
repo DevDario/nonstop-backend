2bcee89afcb72c7bb0d06c05933583c771512f6d
Dário Silva <dariosilva13222@gmail.com>
2024-10-24 20:48:44 +0100

    refactor: changes category typo to CourseCategory

--- src/main/java/ao/com/non_stop/nonstopplatformapi/dtos/CourseDTO.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/dtos/CourseDTO.java
@@ -5,5 +5,5 @@ import ao.com.non_stop.nonstopplatformapi.enums.CourseCategory;
 import java.util.Date;
 import java.util.List;
 
-public record CourseDTO(String name, String description, Date releaseDate, List<CourseCategory> categories, String image) {
+public record CourseDTO(String name, String description, Date releaseDate, CourseCategory category, String image) {
 }
