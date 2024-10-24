7ef223efe3559ba2e628f5218b450480f4118d6d
Dário Silva <dariosilva13222@gmail.com>
2024-10-24 13:48:06 +0100

    feat: finds a course by his id

--- src/main/java/ao/com/non_stop/nonstopplatformapi/services/CourseService.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/services/CourseService.java
@@ -1,12 +1,23 @@
 package ao.com.non_stop.nonstopplatformapi.services;
 
+import ao.com.non_stop.nonstopplatformapi.dtos.CourseDTO;
+import ao.com.non_stop.nonstopplatformapi.entities.Course;
+import ao.com.non_stop.nonstopplatformapi.exceptions.CourseNotFoundException;
 import ao.com.non_stop.nonstopplatformapi.repositories.CourseRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Base64;
 
 @Service
 @AllArgsConstructor
 public class CourseService {
 
     private final CourseRepository courseRepository;
+
+    public Course getCourseById(@PathVariable Long courseId) throws CourseNotFoundException{
+        return courseRepository.findById(courseId).orElseThrow(() -> new CourseNotFoundException(courseId));
+    }
 }
