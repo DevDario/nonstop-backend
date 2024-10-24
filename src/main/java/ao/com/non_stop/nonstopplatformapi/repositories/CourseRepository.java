c89d81c1eb6b26c6411aaa82cc15a208b96061f1
Dário Silva <dariosilva13222@gmail.com>
2024-10-24 13:54:03 +0100

    fix: fixes method signature and adds the requested parameter for name search

--- src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/CourseRepository.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/CourseRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
-    Optional<Course> findByCourseName();
+    Optional<Course> findByName(String name);
 }
