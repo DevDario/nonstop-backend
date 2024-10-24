ccd9253fa06ed97095128df97f747462665f64e8
Dário Silva <dariosilva13222@gmail.com>
2024-10-24 13:25:04 +0100

    feat: creates Course entity controller class

---
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/controllers/CourseController.java
@@ -0,0 +1,11 @@
+package ao.com.non_stop.nonstopplatformapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+}
