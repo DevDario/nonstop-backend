7b81d330ba2c8934375c3e05f9e7309f84791aa1
Dário Silva <dariosilva13222@gmail.com>
2024-10-24 13:49:10 +0100

    chore: adds a endpoint to get a project by his id
    raw: converts the course image to a base64 string

--- src/main/java/ao/com/non_stop/nonstopplatformapi/controllers/CourseController.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/controllers/CourseController.java
@@ -1,11 +1,35 @@
 package ao.com.non_stop.nonstopplatformapi.controllers;
 
+import ao.com.non_stop.nonstopplatformapi.dtos.CourseDTO;
+import ao.com.non_stop.nonstopplatformapi.entities.Course;
+import ao.com.non_stop.nonstopplatformapi.exceptions.CourseNotFoundException;
+import ao.com.non_stop.nonstopplatformapi.services.CourseService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Base64;
 
 @Controller
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CourseController {
+
+    private final CourseService courseService;
+
+    @GetMapping("/{courseId}")
+    public ResponseEntity<CourseDTO> getCourse(@PathVariable Long courseId) throws CourseNotFoundException {
+        Course course = courseService.getCourseById(courseId);
+
+        String base64Image = course.getImage() != null ? Base64.getEncoder().encodeToString(course.getImage().getBytes()) : null;
+
+        CourseDTO courseDTO = new CourseDTO(course.getName(), course.getDescription(), base64Image);
+
+        return ResponseEntity.ok(courseDTO);
+    }
 }
