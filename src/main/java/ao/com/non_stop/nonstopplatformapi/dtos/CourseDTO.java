8f3d81cb9ba23322df72f6fd86c06a675069b0bc
Dário Silva <dariosilva13222@gmail.com>
2024-10-24 17:23:11 +0100

    fix: adds the course category

--- src/main/java/ao/com/non_stop/nonstopplatformapi/dtos/CourseDTO.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/dtos/CourseDTO.java
@@ -1,6 +1,8 @@
 package ao.com.non_stop.nonstopplatformapi.dtos;
 
+import ao.com.non_stop.nonstopplatformapi.enums.CourseCategory;
+
 import java.util.Date;
 
-public record CourseDTO(String name, String description, Date releaseDate, String image) {
+public record CourseDTO(String name, String description, Date releaseDate, CourseCategory category, String image) {
 }
