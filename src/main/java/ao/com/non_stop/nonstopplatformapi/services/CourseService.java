8c274c73766191fc40c9af83882f5526be1d150b
Dário Silva <dariosilva13222@gmail.com>
2024-10-26 01:43:59 +0100

    feat: returns courses based on the specified filters
    raw: transforms the repository response into a pageable object of courses instances
    
    raw: sets default values for filters if they're not specified by the user

--- src/main/java/ao/com/non_stop/nonstopplatformapi/services/CourseService.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/services/CourseService.java
@@ -3,6 +3,7 @@ package ao.com.non_stop.nonstopplatformapi.services;
 import ao.com.non_stop.nonstopplatformapi.dtos.CourseDTO;
 import ao.com.non_stop.nonstopplatformapi.entities.Course;
 import ao.com.non_stop.nonstopplatformapi.enums.CourseCategory;
+import ao.com.non_stop.nonstopplatformapi.enums.CourseLevel;
 import ao.com.non_stop.nonstopplatformapi.exceptions.CourseNotFoundException;
 import ao.com.non_stop.nonstopplatformapi.repositories.CourseRepository;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -55,7 +57,27 @@ public class CourseService {
         return this.courseRepository.findByName(name).orElseThrow(()-> new Exception("This Course Doesn't Exists !"));
     }
 
-    public List<Course> filterCoursesByCategory(CourseCategory category){
-        return this.courseRepository.findByCategory(category);
+
+    public List<CourseDTO> getFilteredCourses(int page, int size, CourseCategory category, CourseLevel level){
+
+        category = (category!=null) ? category:CourseCategory.WEB;
+        level = (level!=null) ? level:CourseLevel.BEGINNER;
+
+        Pageable pageable = PageRequest.of(page,size);
+
+        Page<Course> coursePage = this.courseRepository.findFilteredCourses(
+                category,
+                level,
+                pageable);
+
+        return coursePage.map(course-> new CourseDTO(
+                        course.getName(),
+                        course.getDescription(),
+                        course.getReleaseDate(),
+                        course.getCategory(),
+                        course.getLevel(),
+                        course.getImage()))
+                .stream()
+                .toList();
     }
 }
