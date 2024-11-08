93f4d762a2e0f237b4ebc6d8e5ef3abdd11dd54d
Dário Silva <dariosilva13222@gmail.com>
2024-11-08 09:05:45 +0100

    refactor: removes unused resource

--- src/main/java/ao/com/non_stop/nonstopplatformapi/dtos/CourseDTO.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/dtos/CourseDTO.java
@@ -4,7 +4,6 @@ import ao.com.non_stop.nonstopplatformapi.enums.CourseCategory;
 import ao.com.non_stop.nonstopplatformapi.enums.CourseLevel;
 
 import java.util.Date;
-import java.util.List;
 
 public record CourseDTO(String name, String description, Date releaseDate, CourseCategory category, CourseLevel level, String image) {
 }
