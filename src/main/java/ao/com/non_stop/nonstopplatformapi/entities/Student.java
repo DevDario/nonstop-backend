f938f3a994ddce1b6f6f6add2bc657a2dffd2b84
Dário Silva <dariosilva13222@gmail.com>
2024-11-16 17:40:04 +0100

    feat: creates Student class

---
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/entities/Student.java
@@ -0,0 +1,16 @@
+package ao.com.non_stop.nonstopplatformapi.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student extends User{
+
+}
