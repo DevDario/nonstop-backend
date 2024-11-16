132382a7490a96a3ca5ea4eddcf63f19d5f16f49
Dário Silva <dariosilva13222@gmail.com>
2024-11-16 17:37:45 +0100

    feat: creates User class

---
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/entities/User.java
@@ -0,0 +1,28 @@
+package ao.com.non_stop.nonstopplatformapi.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "username", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 8)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private Date created_at;
+}
