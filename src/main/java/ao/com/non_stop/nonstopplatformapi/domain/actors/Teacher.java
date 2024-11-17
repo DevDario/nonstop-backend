585f139ec1cb6be22222c3995064dc63446415d4
Dário Silva <dariosilva13222@gmail.com>
2024-11-17 12:16:39 +0100

    chore: adds validations to attributes

--- src/main/java/ao/com/non_stop/nonstopplatformapi/domain/actors/Teacher.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/domain/actors/Teacher.java
@@ -3,6 +3,7 @@ package ao.com.non_stop.nonstopplatformapi.domain.actors;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Teacher extends User{
 
-    @Column(name = "about_me", nullable = false, length = 110)
+    @Size(max = 110, message = "About Me cannot exceed 110 characters.")
+    @Column(name = "about_me", nullable = false)
     private String about_me;
 
-    @Column(name = "specialization")
+    @Size(max = 255, message = "Specialization cannot exceed 255 characters.")
+    @Column(name = "specialization", nullable = false)
     private String specialization;
 }
