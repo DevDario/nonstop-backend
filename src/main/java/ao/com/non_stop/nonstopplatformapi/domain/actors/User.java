bba0fa38758238d7b1328212b2f5b5e7e1a06810
Dário Silva <dariosilva13222@gmail.com>
2024-11-17 12:12:13 +0100

    feat: adds validations to attributes

--- src/main/java/ao/com/non_stop/nonstopplatformapi/domain/actors/User.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/domain/actors/User.java
@@ -2,12 +2,18 @@ package ao.com.non_stop.nonstopplatformapi.domain.actors;
 
 import ao.com.non_stop.nonstopplatformapi.enums.Roles;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
 public abstract class User{
 
     @Id
@@ -17,16 +23,21 @@ public abstract class User{
     @Column(name = "username", nullable = false)
     private String name;
 
+    @NotNull
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "password", nullable = false, length = 8)
+    @NotNull
+    @Size(min = 8, max = 12, message = "Password must have at least 8 charachters")
+    @Column(name = "password", nullable = false)
     private String password;
 
+    @NotNull
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     private Roles role;
 
+    @NotNull
     @CreationTimestamp
     @Column(name = "created_at", nullable = false)
     private Date created_at;
