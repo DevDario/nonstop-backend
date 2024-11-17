76d3e79068fbcb5b3dc1a254a09c0fc116c79ca5
Dário Silva <dariosilva13222@gmail.com>
2024-11-17 12:40:47 +0100

    feat: adds validation to attributes

--- src/main/java/ao/com/non_stop/nonstopplatformapi/domain/entities/Enrollment.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/domain/entities/Enrollment.java
@@ -2,6 +2,10 @@ package ao.com.non_stop.nonstopplatformapi.domain.entities;
 
 import ao.com.non_stop.nonstopplatformapi.domain.actors.Student;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,17 +30,20 @@ public class Enrollment {
     @Column(name = "enrollment_date", nullable = false)
     private Date enrollment_date;
 
+    @NotBlank
+    @Min(0)
+    @Max(100)
     @Column(name = "progress_percentage", nullable = false)
     private Float progress;
 
     @Column(name = "is_completed", nullable = false)
-    private Boolean is_completed;
+    private Boolean is_completed = false;
 
     @ManyToOne
     @JoinColumn(name = "student_id", nullable = false)
-    private Student student_id;
+    private Student student;
 
     @ManyToOne
     @JoinColumn(name = "course_id", nullable = false)
-    private Course course_id;
+    private Course course;
 }
