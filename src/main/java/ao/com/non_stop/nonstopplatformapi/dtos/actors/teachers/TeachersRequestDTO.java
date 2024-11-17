86e53c8e9ba936e106bc8a6845dd1bbde3869df1
Dário Silva <dariosilva13222@gmail.com>
2024-11-17 19:10:48 +0100

    feat: creates request DTO for Teacher Entity

---
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/dtos/actors/teachers/TeachersRequestDTO.java
@@ -0,0 +1,4 @@
+package ao.com.non_stop.nonstopplatformapi.dtos.actors.teachers;
+
+public record TeachersRequestDTO(String name, String email, String password, String about, String specialization) {
+}
