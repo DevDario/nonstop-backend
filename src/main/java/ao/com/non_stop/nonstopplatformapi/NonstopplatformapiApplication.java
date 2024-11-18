d3b61967e17efebab52600941801abb597d1cd5c
Dário Silva <dariosilva13222@gmail.com>
2024-11-18 16:16:53 +0100

    build: adds support to env files

--- src/main/java/ao/com/non_stop/nonstopplatformapi/NonstopplatformapiApplication.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/NonstopplatformapiApplication.java
@@ -2,8 +2,10 @@ package ao.com.non_stop.nonstopplatformapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/.env")
 public class NonstopplatformapiApplication {
 
 	public static void main(String[] args) {
