28ace0abf2ca23310c01ad5188cac9d02fe25344
Dário Silva <dariosilva13222@gmail.com>
2024-11-18 16:33:26 +0100

    feat: creates methods to extract information from tokens, and validate them

--- src/main/java/ao/com/non_stop/nonstopplatformapi/infra/security/TokenService.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/infra/security/TokenService.java
@@ -1,15 +1,15 @@
 package ao.com.non_stop.nonstopplatformapi.infra.security;
 
+import ao.com.non_stop.nonstopplatformapi.domain.actors.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.time.Instant;
 import java.util.Date;
 import java.util.Map;
 
@@ -22,18 +22,46 @@ public class TokenService {
     @Value("${security.jwt.expiration-time}")
     private long expirationDate;
 
-    public String generateToken(Map<String, Object> claims, UserDetails userDetails){
+    public String generateToken(Map<String, Object> claims, User user){
         return Jwts.builder()
                 .setClaims(claims)
-                .setSubject(userDetails.getUsername())
+                .setSubject(user.getEmail())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationDate))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
+    public boolean isTokenValid(String token, User user){
+        String email = extractEmail(token);
+        return (email.equals(user.getEmail()) && !isTokenExpired(token));
+    }
+
     public Key getSignInKey(){
         byte[] keyBytes = Decoders.BASE64URL.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getExpiration();
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private String extractEmail(String token){
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
     }
 
 }
