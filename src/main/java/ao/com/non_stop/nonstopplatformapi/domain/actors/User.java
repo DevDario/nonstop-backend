73569eed4ad5ccab92d8fd18b19a94e2bcf188d8
Dário Silva <dariosilva13222@gmail.com>
2024-11-21 08:54:00 +0100

    refactor: removes unused import

--- src/main/java/ao/com/non_stop/nonstopplatformapi/domain/actors/User.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/domain/actors/User.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.util.Date;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
