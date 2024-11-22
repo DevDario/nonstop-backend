d05ef837c89660f342c13eb0ceee82943d33fce6
Dário Silva <dariosilva13222@gmail.com>
2024-11-22 03:05:28 +0100

    fix: annotates class with @Service and remove unnecessary attribute

--- src/main/java/ao/com/non_stop/nonstopplatformapi/services/CustomUserDetailsService.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/services/CustomUserDetailsService.java
@@ -4,26 +4,20 @@ import ao.com.non_stop.nonstopplatformapi.domain.actors.User;
 import ao.com.non_stop.nonstopplatformapi.infra.security.CustomUserDetails;
 import ao.com.non_stop.nonstopplatformapi.repositories.UsersRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
     private UsersRepository usersRepository;
-
-    @Autowired
-    CustomUserDetails customUserDetails;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = usersRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("User not found with email " + username));
-
-        return new CustomUserDetails();
+        return new CustomUserDetails(user);
     }
 }
