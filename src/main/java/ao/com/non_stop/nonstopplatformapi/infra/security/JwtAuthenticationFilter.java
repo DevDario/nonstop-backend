1faa2e03c8e6669b48c92151e555e035bd36c4e6
Dário Silva <dariosilva13222@gmail.com>
2024-11-28 06:18:22 +0100

    feat: validates the token using the current authenticated user

--- src/main/java/ao/com/non_stop/nonstopplatformapi/infra/security/JwtAuthenticationFilter.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/infra/security/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package ao.com.non_stop.nonstopplatformapi.infra.security;
 
+import ao.com.non_stop.nonstopplatformapi.domain.actors.User;
+import ao.com.non_stop.nonstopplatformapi.repositories.UsersRepository;
 import ao.com.non_stop.nonstopplatformapi.services.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -9,6 +11,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,27 +21,37 @@ import java.io.IOException;
 @AllArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    TokenService tokenService;
-    CustomUserDetailsService customUserDetailsService;
+    private final TokenService tokenService;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final UsersRepository usersRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token = this.extractToken(request);
-        String email = this.tokenService.validateToken(token);
+        try {
+            String token = this.extractToken(request);
 
-        if(email != null){
-            UserDetails userDetails = this.customUserDetailsService.loadUserByUsername(email);
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            if (token != null) {
+                String email = this.tokenService.extractEmail(token);
+                User currrentUser = this.usersRepository.findByEmail(email).orElseThrow();
+
+                if(this.tokenService.isTokenValid(token,currrentUser)) {
+                    UserDetails userDetails = this.customUserDetailsService.loadUserByUsername(email);
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails((new WebAuthenticationDetailsSource().buildDetails(request)));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
         }
-
-        filterChain.doFilter(request,response);
-
     }
 
     private String extractToken(HttpServletRequest request){
         String authHeader = request.getHeader("Authorization");
-        if(authHeader == null) return null;
-        return authHeader.replace("Bearer ","");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) return null;
+        return authHeader.substring(7);
     }
 }
