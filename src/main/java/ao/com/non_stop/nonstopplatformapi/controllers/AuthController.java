ca0727e89178c614774858bf743a4e3824d08a5a
Dário Silva <dariosilva13222@gmail.com>
2024-11-28 06:14:29 +0100

    feat: returns a body using the ResponseDTO

--- src/main/java/ao/com/non_stop/nonstopplatformapi/controllers/AuthController.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/controllers/AuthController.java
@@ -1,18 +1,14 @@
 package ao.com.non_stop.nonstopplatformapi.controllers;
 
-import ao.com.non_stop.nonstopplatformapi.domain.actors.Admin;
-import ao.com.non_stop.nonstopplatformapi.domain.actors.Student;
-import ao.com.non_stop.nonstopplatformapi.domain.actors.Teacher;
 import ao.com.non_stop.nonstopplatformapi.domain.actors.User;
 import ao.com.non_stop.nonstopplatformapi.dtos.LoginRequestDTO;
 import ao.com.non_stop.nonstopplatformapi.dtos.RegisterRequestDTO;
 import ao.com.non_stop.nonstopplatformapi.dtos.ResponseDTO;
-import ao.com.non_stop.nonstopplatformapi.enums.Roles;
 import ao.com.non_stop.nonstopplatformapi.infra.security.TokenService;
-import ao.com.non_stop.nonstopplatformapi.repositories.UsersRepository;
+import ao.com.non_stop.nonstopplatformapi.services.AuthenticationService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,62 +16,21 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AuthController {
 
-    private UsersRepository userRepository;
-    private PasswordEncoder passwordEncoder;
     private TokenService tokenService;
+    private AuthenticationService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> registerUser(@RequestBody RegisterRequestDTO registerDTO) {
+    public ResponseEntity<ResponseDTO> registerUser(@RequestBody RegisterRequestDTO registerDTO) throws Exception{
+        User authenticatedUser = this.authService.signup(registerDTO);
 
-        // Check if the email is already taken
-        if (userRepository.findByEmail(registerDTO.email()).isPresent()) {
-            return ResponseEntity.badRequest().body("Error: Email is already in use!");
-        }
-
-        // Determine the role and create the corresponding user
-        User newUser;
-        switch (registerDTO.role()) {
-            case Roles.ADMIN:
-                newUser = new Admin();
-                newUser.setName(registerDTO.name());
-                newUser.setEmail(registerDTO.email());
-                newUser.setRole(registerDTO.role());
-                newUser.setPassword(passwordEncoder.encode(registerDTO.password()));
-                break;
-
-            case Roles.TEACHER:
-                newUser = new Teacher();
-                newUser.setName(registerDTO.name());
-                newUser.setEmail(registerDTO.email());
-                newUser.setRole(registerDTO.role());
-                newUser.setPassword(passwordEncoder.encode(registerDTO.password()));
-                break;
-
-            case Roles.STUDENT:
-                newUser = new Student();
-                newUser.setName(registerDTO.name());
-                newUser.setEmail(registerDTO.email());
-                newUser.setRole(registerDTO.role());
-                newUser.setPassword(passwordEncoder.encode(registerDTO.password()));
-                break;
-
-            default:
-                return ResponseEntity.badRequest().body("Error: Invalid role specified!");
-        }
-
-        // Save the user to the database
-        String token = this.tokenService.generateToken(newUser,registerDTO.role().name());
-        userRepository.save(newUser);
-        return ResponseEntity.ok().body(token);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDTO(authenticatedUser.getName(), null));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ResponseDTO> login(@RequestBody LoginRequestDTO loginDTO){
-        User user = this.userRepository.findByEmail(loginDTO.email()).orElseThrow(()-> new RuntimeException("User not found with email " + loginDTO.email()));
-        if(passwordEncoder.matches(loginDTO.password(), user.getPassword())){
-            String token = this.tokenService.generateToken(user,user.getRole().name());
-            return ResponseEntity.ok().body( new ResponseDTO(user.getName(),token));
-        }
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ResponseDTO> login(@RequestBody LoginRequestDTO loginDTO) throws Exception{
+        User newUser = authService.login(loginDTO);
+        String token = this.tokenService.generateToken(newUser);
+
+        return ResponseEntity.ok().body(new ResponseDTO(newUser.getEmail(), token));
     }
 }
