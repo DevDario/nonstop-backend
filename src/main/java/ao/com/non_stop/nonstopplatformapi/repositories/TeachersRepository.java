fca3477e24d6a488b3f86d783dc7393e2c8ec82b
Dário Silva <dariosilva13222@gmail.com>
2024-11-28 08:54:32 +0100

    feat: finds teachers by their email

--- src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/TeachersRepository.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/TeachersRepository.java
@@ -3,5 +3,8 @@ package ao.com.non_stop.nonstopplatformapi.repositories;
 import ao.com.non_stop.nonstopplatformapi.domain.actors.Teacher;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TeachersRepository extends CrudRepository<Teacher,String> {
+    Optional<Teacher> findByEmail(String email);
 }
