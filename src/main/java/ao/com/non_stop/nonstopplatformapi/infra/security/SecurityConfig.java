6edc0246a0eae2bfdbce925270a913c69cb73ab5
Dário Silva <dariosilva13222@gmail.com>
2024-11-28 06:17:22 +0100

    feat: adds login and logout endpoints to unsecured requests

--- src/main/java/ao/com/non_stop/nonstopplatformapi/infra/security/SecurityConfig.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/infra/security/SecurityConfig.java
@@ -1,19 +1,15 @@
 package ao.com.non_stop.nonstopplatformapi.infra.security;
 
-import ao.com.non_stop.nonstopplatformapi.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,9 +18,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    CustomUserDetailsService userDetailsService;
     @Autowired
     JwtAuthenticationFilter jwtAuthenticationFilter;
 
@@ -40,6 +33,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET,"/api/v1/course/name/").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/course/filter/").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/course/all").permitAll()
+                        .requestMatchers("/login/**").permitAll()
+                        .requestMatchers("/logout/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
