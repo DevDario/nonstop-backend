e1459c8f109f21935dec6cd7a1030f7e62832660
Dário Silva <dariosilva13222@gmail.com>
2024-11-28 08:57:21 +0100

    feat: adds new attributes related to the Teacher Entity

--- src/main/java/ao/com/non_stop/nonstopplatformapi/dtos/RegisterRequestDTO.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/dtos/RegisterRequestDTO.java
@@ -2,5 +2,5 @@ package ao.com.non_stop.nonstopplatformapi.dtos;
 
 import ao.com.non_stop.nonstopplatformapi.enums.Roles;
 
-public record RegisterRequestDTO(String name, String email, Roles role, String password) {
+public record RegisterRequestDTO(String name, String email,String about_me, String specialization ,Roles role, String password) {
 }
