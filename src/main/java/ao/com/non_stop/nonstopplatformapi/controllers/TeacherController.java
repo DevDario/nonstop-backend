192a0f75c8bf4fea76ea8f13d69490451ba655b2
Dário Silva <dariosilva13222@gmail.com>
2024-12-03 07:04:32 +0100

    chore: maps endpoints for CRUD operations

--- src/main/java/ao/com/non_stop/nonstopplatformapi/controllers/TeacherController.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/controllers/TeacherController.java
@@ -1,13 +1,15 @@
 package ao.com.non_stop.nonstopplatformapi.controllers;
 
+import ao.com.non_stop.nonstopplatformapi.dtos.course.CourseOverviewResponseDTO;
 import ao.com.non_stop.nonstopplatformapi.dtos.course.CourseRequestDTO;
 import ao.com.non_stop.nonstopplatformapi.services.CourseService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RestController
@@ -23,8 +25,17 @@ public class TeacherController {
     }
 
     @DeleteMapping("/{course_id}")
-    public ResponseEntity<String> deleteCourse(@AuthenticationPrincipal Authentication principal, @PathVariable long course_id) throws Exception{
+    public ResponseEntity<String> deleteCourse(Principal principal, @PathVariable long course_id) throws Exception{
         String email = principal.getName();
-        return this.courseService.deleteCourseByTeacher(email,course_id);
+        return this.courseService.deleteCourseFromTeacher(email,course_id);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<CourseOverviewResponseDTO>> getAllCourses(Principal principal,
+                                                                         @RequestParam(defaultValue = "0") int page,
+                                                                         @RequestParam(defaultValue = "5") int size) throws  Exception{
+        String email = principal.getName();
+        List<CourseOverviewResponseDTO> allCourses = this.courseService.getAllCoursesFromTeacher(page,size,email);
+        return ResponseEntity.ok(allCourses);
     }
 }
