e3173b105e2724505b70ac478a6235cf682a6487
Dário Silva <dariosilva13222@gmail.com>
2024-12-04 20:00:41 +0100

    chore: returns a list of courses where the student is enrolled in

--- src/main/java/ao/com/non_stop/nonstopplatformapi/services/StudentService.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/services/StudentService.java
@@ -1,6 +1,9 @@
 package ao.com.non_stop.nonstopplatformapi.services;
 
 import ao.com.non_stop.nonstopplatformapi.domain.actors.Student;
+import ao.com.non_stop.nonstopplatformapi.domain.entities.Course;
+import ao.com.non_stop.nonstopplatformapi.domain.entities.Enrollment;
+import ao.com.non_stop.nonstopplatformapi.repositories.EnrollmentRepository;
 import ao.com.non_stop.nonstopplatformapi.repositories.StudentsRepository;
 import ao.com.non_stop.nonstopplatformapi.repositories.UsersRepository;
 import lombok.AllArgsConstructor;
@@ -10,12 +13,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class StudentService {
 
-    private StudentsRepository studentsRepository;
-    private UsersRepository usersRepository;
+    private final StudentsRepository studentsRepository;
+    private final UsersRepository usersRepository;
+    private final EnrollmentRepository enrollmentRepository;
 
 
     @Transactional
@@ -29,5 +35,12 @@ public class StudentService {
         } catch (UsernameNotFoundException e) {
             throw new UsernameNotFoundException("There's no Student with ["+email+"]email !");
         }
+    }
+
+    public ResponseEntity<List<Course>> getAllEnrollments(String email){
+        Student student = (Student) this.usersRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("No Student Were Found !"));
+        List<Course> enrollments = this.enrollmentRepository.findByStudent(student).stream().map(Enrollment::getCourse).toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(enrollments);
     }
 }
