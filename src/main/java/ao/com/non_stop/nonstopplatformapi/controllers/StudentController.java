6c16a55bba3f4d5c9dd22fb569a37f69998f8fea
Dário Silva <dariosilva13222@gmail.com>
2024-12-04 20:00:09 +0100

    chore: finds user(student) and returns a list of courses

--- src/main/java/ao/com/non_stop/nonstopplatformapi/controllers/StudentController.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/controllers/StudentController.java
@@ -1,14 +1,18 @@
 package ao.com.non_stop.nonstopplatformapi.controllers;
 
+import ao.com.non_stop.nonstopplatformapi.domain.entities.Course;
 import ao.com.non_stop.nonstopplatformapi.dtos.course.CourseResponseDTO;
 import ao.com.non_stop.nonstopplatformapi.services.CourseService;
+import ao.com.non_stop.nonstopplatformapi.services.StudentService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -17,10 +21,21 @@ import java.util.List;
 @RequestMapping("/api/v1/student")
 public class StudentController {
 
-    CourseService courseService;
+    private final CourseService courseService;
+    private final StudentService studentService;
 
     @GetMapping("/")
     public List<CourseResponseDTO> getAllCoursesFromPlatform(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "15") int size){
         return this.courseService.getAllCourses(page, size);
+    }
+
+    @GetMapping("/enrollments")
+    public ResponseEntity<List<Course>> getAllEnrolledCourses(Principal principal){
+        String email = this.getStudentEmail(principal);
+        return this.studentService.getAllEnrollments(email);
+    }
+
+    private String getStudentEmail(Principal principal){
+        return principal.getName();
     }
 }
