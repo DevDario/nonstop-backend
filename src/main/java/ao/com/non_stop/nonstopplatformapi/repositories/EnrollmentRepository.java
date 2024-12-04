2af6e3a0f5b086a0addbef997e7baac7280a69eb
Dário Silva <dariosilva13222@gmail.com>
2024-12-04 19:58:31 +0100

    feat: finds Enrollment by student

--- src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/EnrollmentRepository.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/EnrollmentRepository.java
@@ -1,9 +1,15 @@
 package ao.com.non_stop.nonstopplatformapi.repositories;
 
+import ao.com.non_stop.nonstopplatformapi.domain.actors.Student;
 import ao.com.non_stop.nonstopplatformapi.domain.entities.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
+
+    List<Enrollment> findByStudent(Student student);
+
 }
