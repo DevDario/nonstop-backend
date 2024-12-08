458c9ceab7efd01a1d5b547efecc16d29734115b
Dário Silva <dariosilva13222@gmail.com>
2024-12-08 16:24:21 +0100

    chore: creates Course Classes Repository

---
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/ClassesRepository.java
@@ -0,0 +1,9 @@
+package ao.com.non_stop.nonstopplatformapi.repositories;
+
+import ao.com.non_stop.nonstopplatformapi.domain.entities.CourseClasses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClassesRepository extends JpaRepository<CourseClasses,Long> {
+}
