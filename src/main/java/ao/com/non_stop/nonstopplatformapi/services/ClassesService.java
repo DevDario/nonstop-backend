8c0eac4e35b14b313f35b09e06751a05f17b034a
Dário Silva <dariosilva13222@gmail.com>
2024-12-08 14:54:45 +0100

    feat: handles operations related to course classes

---
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/services/ClassesService.java
@@ -0,0 +1,9 @@
+package ao.com.non_stop.nonstopplatformapi.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ClassesService {
+}
