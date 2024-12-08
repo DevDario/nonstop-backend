7342fa26dd16d20c34fc3d77912aac785fefc38f
Dário Silva <dariosilva13222@gmail.com>
2024-12-08 16:39:25 +0100

    feat: finds a class based on the given course

--- src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/ClassesRepository.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/ClassesRepository.java
@@ -1,9 +1,12 @@
 package ao.com.non_stop.nonstopplatformapi.repositories;
 
+import ao.com.non_stop.nonstopplatformapi.domain.entities.Course;
 import ao.com.non_stop.nonstopplatformapi.domain.entities.CourseClasses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClassesRepository extends JpaRepository<CourseClasses,Long> {
+
+    CourseClasses findByCourseAndId(Course course,long id);
 }
