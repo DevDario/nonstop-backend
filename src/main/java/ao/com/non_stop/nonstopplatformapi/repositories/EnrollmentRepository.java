9e866c637e4516225f7156d376324d4d959a9812
Dário Silva <dariosilva13222@gmail.com>
2024-12-16 19:42:30 +0100

    feat: finds an Enrollment based on the course and student

--- src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/EnrollmentRepository.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/EnrollmentRepository.java
@@ -1,15 +1,18 @@
 package ao.com.non_stop.nonstopplatformapi.repositories;
 
 import ao.com.non_stop.nonstopplatformapi.domain.actors.Student;
+import ao.com.non_stop.nonstopplatformapi.domain.entities.Course;
 import ao.com.non_stop.nonstopplatformapi.domain.entities.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
 
     List<Enrollment> findByStudent(Student student);
 
+    Optional<Enrollment> findByStudentAndCourse(Student student, Course course);
 }
