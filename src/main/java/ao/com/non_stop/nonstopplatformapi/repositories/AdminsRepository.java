0c5e63f96066f5628415e6f433e45f80a17cef19
Dário Silva <dariosilva13222@gmail.com>
2025-01-02 14:28:31 +0100

    feat: find admin users by Email

--- src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/AdminsRepository.java
+++ src/main/java/ao/com/non_stop/nonstopplatformapi/repositories/AdminsRepository.java
@@ -4,6 +4,10 @@ import ao.com.non_stop.nonstopplatformapi.domain.actors.Admin;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminsRepository extends CrudRepository<Admin, String> {
+
+    Optional<Admin> findByEmail(String email);
 }
